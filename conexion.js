c52da508aaf82dedabc50bc14804cc0056f2765c
vale-439 <2016439@est.cedesdonbosco.ed.cr>
2026-07-30 08:39:02 -0600

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
-import dotenv from 'dotenv';
-dotenv.config();
-const uri = process.env.MONGO;
+const dotenv = require('dotenv');
 
-//const uri = 'mongodb+srv://Val:97801941_Vb@cluster0.dennbyw.mongodb.net/?appName=Cluster0';
+dotenv.config
+
+const uri = process.env.MONGO
+
+
 
 async function conectarBD() {
 try {
 await mongoose.connect(uri);
-console.log('Conectado correctamente a MongoDB Atlas con Mongoose');
+console.log('Conectado correctamente a MongoDB Atlas con Mongoose actualizado');
 } catch (error) {
 console.log('Error conectando con Mongoose:', error.message);
 }
